5a75ab3929bece2a673472dc74331dabf2be04fa
Mikael Wahlberg <wahlberg.micke@gmail.com>
2017-01-17 06:37:34 +0100

    Added date validation

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -18,50 +18,52 @@ $( ".hamburger" ).show();
 
 // CALCULATE BUTTON
 $( "#calculate" ).click(function() {
-	$('.loading').show();
 
 	var ticker = $('input[name=ticker]').val();
 	var startDate = $('input[name=startDate]').val();
 	var endDate = $('input[name=endDate]').val();
 
-	if (!isValidDate(startDate) || !isValidDate(endDate)) {
-		$('.loading').hide();
-		$( ".popup" ).show();
-		$( ".popup p" ).show();
-    	$( ".popup p" ).text( 'Please enter the correct date format and try again.');
-	} else {
+	if (validateDate(startDate, endDate)) {
+		$('.loading').show();
 		$.ajax({
 		        type: "POST",
 		        url: "garch/python",
 		        data: {t: ticker, s: startDate, e: endDate},
 		        success: callbackFunc
 		});
-
-		function callbackFunc(response) {
-			var responseMessage = response[0];
-			console.log(responseMessage);
-			if (response[1] == 'False') {
-				$( ".popup" ).show();
-				$( ".popup img" ).hide();
-				$( ".popup p" ).show();
-		    	$( ".popup p" ).text(responseMessage + ' Please try again with correct input.');
-
-			} else {
-				$( ".popup" ).show();
-				$( ".popup img" ).attr('src', responseMessage);
-				$( ".popup img" ).show();
-		    	$( ".popup p" ).hide();	
-			};
-
-			$('.loading').hide();
-		    
-		};
 	};
 });
 
+function callbackFunc(response) {
+	var responseMessage = response[0];
+	if (response[1] == 'False') {
+		$( ".popup" ).show();
+		$( ".popup img" ).hide();
+		$( ".popup p" ).show();
+    	$( ".popup p" ).text(responseMessage + ' Please try again with correct input.');
+	} else {
+		$( ".popup" ).show();
+		$( ".popup img" ).attr('src', responseMessage);
+		$( ".popup img" ).show();
+    	$( ".popup p" ).hide();	
+	};
+	$('.loading').hide();    
+};
+
+function validateDate(startDate, endDate) {
+	if (!isValidDate(startDate) || !isValidDate(endDate) || !startDateBeforeEndDate(startDate, endDate)) {
+		$('.loading').hide();
+		$( ".popup" ).show();
+		$( ".popup p" ).show();
+    	$( ".popup p" ).text( 'Please enter the correct date format and try again.');
+    	return false;
+    } else {
+    	return true;
+    };
+};
+
 // VALIDATE DATE
-function isValidDate(dateString)
-{
+function isValidDate(dateString) {
     // First check for the pattern
     var regex_date = /^\d{4}\-\d{1,2}\-\d{1,2}$/;
 
@@ -92,6 +94,18 @@ function isValidDate(dateString)
 
     // Check the range of the day
     return day > 0 && day <= monthLength[month - 1];
-}
+};
 
-//!function(a,b){function c(a,b){var c=a.createElement("p"),d=a.getElementsByTagName("head")[0]||a.documentElement;return c.innerHTML="x<style>"+b+"</style>",d.insertBefore(c.lastChild,d.firstChild)}function d(){var a=s.elements;return"string"==typeof a?a.split(" "):a}function e(a){var b=r[a[p]];return b||(b={},q++,a[p]=q,r[q]=b),b}function f(a,c,d){if(c||(c=b),k)return c.createElement(a);d||(d=e(c));var f;return f=d.cache[a]?d.cache[a].cloneNode():o.test(a)?(d.cache[a]=d.createElem(a)).cloneNode():d.createElem(a),!f.canHaveChildren||n.test(a)||f.tagUrn?f:d.frag.appendChild(f)}function g(a,c){if(a||(a=b),k)return a.createDocumentFragment();c=c||e(a);for(var f=c.frag.cloneNode(),g=0,h=d(),i=h.length;i>g;g++)f.createElement(h[g]);return f}function h(a,b){b.cache||(b.cache={},b.createElem=a.createElement,b.createFrag=a.createDocumentFragment,b.frag=b.createFrag()),a.createElement=function(c){return s.shivMethods?f(c,a,b):b.createElem(c)},a.createDocumentFragment=Function("h,f","return function(){var n=f.cloneNode(),c=n.createElement;h.shivMethods&&("+d().join().replace(/[\w\-:]+/g,function(a){return b.createElem(a),b.frag.createElement(a),'c("'+a+'")'})+");return n}")(s,b.frag)}function i(a){a||(a=b);var d=e(a);return!s.shivCSS||j||d.hasCSS||(d.hasCSS=!!c(a,"article,aside,dialog,figcaption,figure,footer,header,hgroup,main,nav,section{display:block}mark{background:#FF0;color:#000}template{display:none}")),k||h(a,d),a}var j,k,l="3.7.0",m=a.html5||{},n=/^<|^(?:button|map|select|textarea|object|iframe|option|optgroup)$/i,o=/^(?:a|b|code|div|fieldset|h1|h2|h3|h4|h5|h6|i|label|li|ol|p|q|span|strong|style|table|tbody|td|th|tr|ul)$/i,p="_html5shiv",q=0,r={};!function(){try{var a=b.createElement("a");a.innerHTML="<xyz></xyz>",j="hidden"in a,k=1==a.childNodes.length||function(){b.createElement("a");var a=b.createDocumentFragment();return"undefined"==typeof a.cloneNode||"undefined"==typeof a.createDocumentFragment||"undefined"==typeof a.createElement}()}catch(c){j=!0,k=!0}}();var s={elements:m.elements||"abbr article aside audio bdi canvas data datalist details dialog figcaption figure footer header hgroup main mark meter nav output progress section summary template time video",version:l,shivCSS:m.shivCSS!==!1,supportsUnknownElements:k,shivMethods:m.shivMethods!==!1,type:"default",shivDocument:i,createElement:f,createDocumentFragment:g};a.html5=s,i(b)}(this,document);
+function startDateBeforeEndDate(startDate, endDate) {
+	var startParts   = startDate.split("-");
+    var startDay     = parseInt(startParts[2], 10);
+    var startMonth   = parseInt(startParts[1], 10);
+    var startYear    = parseInt(startParts[0], 10);
+
+    var endParts   = endDate.split("-");
+    var endDay     = parseInt(endParts[2], 10);
+    var endMonth   = parseInt(endParts[1], 10);
+    var endYear    = parseInt(endParts[0], 10);
+
+    return (endYear + endMonth + endDay) > (startYear + startMonth + startDay);
+};
